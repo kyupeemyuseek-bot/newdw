897f6a930967fc83b1dfcbb53c545224fdd26c0b
kyupeemyuseek@gmail.com <kyupeemyuseek@gmail.com>
2025-08-20 09:36:36 +0000

    dedupe

--- models/staging/stg_account_status.sql
+++ models/staging/stg_account_status.sql
@@ -2,12 +2,21 @@ with source as (
     select * from {{ ref('AccountStatus') }}
 ),
 
+deduped as (
+    select *
+    from source
+    qualify row_number() over (
+        partition by StatusCode 
+        order by CreatedDate desc
+    ) = 1
+),
+
 renamed as (
     select
         cast(StatusCode as integer)       as status_code,
         trim(Description)                 as status_description,
         cast(CreatedDate as date)         as created_date
-    from source
+    from deduped
 )
 
 select * from renamed
