be09b83c5cab0ccbd620ab2f3acf6b5fbea46297
Angelo Alfonso <angeloalfonso.msit@gmail.com>
2025-08-23 06:58:49 +0000

    fact_reservationhdr

--- models/marts/fact_reservations.sql
+++ models/marts/fact_reservations.sql
@@ -4,28 +4,60 @@
     schema = 'marts'
 ) }}
 
+with reservation as (
+    select
+        -- business identifiers
+        transaction_id,
+        branch_id,
+        client_id,
+        unit_code,
+
+        -- dimension keys (foreign keys)
+        status_code,
+        finance_type_code,
+        source_code,
+
+        -- measures
+        lot_price,
+        vat,
+        gross_lot_price,
+        sales_disc,
+        net_lot_price,
+        misc,
+        tcp,
+
+        -- dates
+        reservation_date
+    from {{ ref('stg_reservationhdr') }}
+),
+
+lotinventory as (
+    select
+        unit_code,
+        unit_cost
+    from {{ ref('stg_lotinventory') }}
+)
+
 select
-    -- business identifiers
-    transaction_id,
-    branch_id,
-    client_id,
-    unit_code,
+    r.transaction_id,
+    r.branch_id,
+    r.client_id,
+    r.unit_code,
+    r.status_code,
+    r.finance_type_code,
+    r.source_code,
+    r.lot_price,
+    r.vat,
+    r.gross_lot_price,
+    r.sales_disc,
+    r.net_lot_price,
+    r.misc,
+    r.tcp,
+    r.reservation_date,
 
-    -- dimension keys (foreign keys)
-    status_code,
-    finance_type_code,
-    source_code,
+    -- new column
+    (r.net_lot_price - li.unit_cost) as revenue
 
-    -- measures
-    lot_price,
-    vat,
-    gross_lot_price,
-    sales_disc,
-    net_lot_price,
-    misc,
-    tcp,
-
-    -- dates
-    reservation_date
-
-from {{ ref('stg_reservationhdr') }}
+from reservation r
+left join lotinventory li
+    on r.unit_code = li.unit_code
